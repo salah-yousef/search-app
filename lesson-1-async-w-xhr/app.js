19f36beb5b2f4fa74b2590aced873577d405d4e7
salah yousef <simyousef@gmail.com>
2018-07-09 19:37:27 +0200

    fix indentation

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -15,14 +15,12 @@
         articleRequest.onload = addArticles;
         articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=742f63e717a9401ebc334310cdf5c37d`);
         articleRequest.send();
-        
+
         const unsplashRequest = new XMLHttpRequest();
         unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
         unsplashRequest.onload = addImage;
         unsplashRequest.setRequestHeader('Authorization', 'Client-ID dcfe3a0e0b3631911d31eb1c88ebac9cf292bd1b2e9140bfc46da8bbff997f2a');
         unsplashRequest.send();
-
-
 
         function addImage() {
             let htmlContent = document.createElement('div');
