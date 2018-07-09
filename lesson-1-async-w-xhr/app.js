8580b2d5d13bdec26d58ab9c8698a7ed172be2b4
salah yousef <simyousef@gmail.com>
2018-07-09 19:04:48 +0200

    order fix

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -9,15 +9,13 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
- 
- 
         searchedForText = document.getElementById('search-keyword').value;
- 
+
         const articleRequest = new XMLHttpRequest();
         articleRequest.onload = addArticles;
         articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=742f63e717a9401ebc334310cdf5c37d`);
         articleRequest.send();
- 
+        
         const unsplashRequest = new XMLHttpRequest();
         unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
         unsplashRequest.onload = addImage;
@@ -26,22 +24,6 @@
 
 
 
-        function addArticles() {
-            let htmlContent = document.createElement('div');
-            const data = JSON.parse(this.responseText);
-            if (data.response && data.response.docs && data.response.docs.length > 1) {
-                htmlContent.innerHTML = '<ul>' + data.response.docs.map(article => `<li class="article"> <h2> <a href="${article.web_url}"> ${article.headline.main} </a> </h2>
-                <p>${article.snippet}</p>
-                </li>`
-                ).join('') + '</ul>';
-            } else {
-                htmlContent.innerHTML = '<div class="error-no-articles"> No articles available </div>';
-            }
-            responseContainer.insertAdjacentElement('afterbegin', htmlContent);
-
-        }
-
-        
         function addImage() {
             let htmlContent = document.createElement('div');
             const data = JSON.parse(this.responseText);
@@ -58,13 +40,22 @@
             responseContainer.insertAdjacentElement('afterbegin', htmlContent);
         }
 
+        function addArticles() {
+            let htmlContent = document.createElement('div');
+            const data = JSON.parse(this.responseText);
+            if (data.response && data.response.docs && data.response.docs.length > 1) {
+                htmlContent.innerHTML = '<ul>' + data.response.docs.map(article => `<li class="article"> <h2> <a href="${article.web_url}"> ${article.headline.main} </a> </h2>
+                <p>${article.snippet}</p>
+                </li>`
+                ).join('') + '</ul>';
+            } else {
+                htmlContent.innerHTML = '<div class="error-no-articles"> No articles available </div>';
+            }
+            responseContainer.insertAdjacentElement('beforeend', htmlContent);
 
- 
-
+        }
 
 
     });
-
-
 
 })();
